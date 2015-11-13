88e6edd7b3da4fa80732e8beab8fdfd8ee8f402b
rosemschneider <roseschneider@sr13-fb1fc86325.stanford.edu>
2015-11-13 14:07:36 -0800

    working on rounding to whole numbers

---
+++ tablet_exp/sprite_whole.js
@@ -0,0 +1,184 @@
+var SpriteData = {
+	someapple: {
+		begin: 0.000,
+		onset: 4.224,
+		length: 5.000
+	},
+	noneapple: {
+		begin: 6.000,
+		onset: 10.414,
+		length: 5.675
+	},
+	allapple: {
+		begin: 12.000,
+		onset: 16.879,
+		length: 6.000
+	},
+	somebanana: {
+		begin: 19.000,
+		onset: 23.390,
+		length: 5.000
+	}, 
+	nonebanana: {
+		begin: 25.000,
+		onset: 29.949,
+		length: 6.000,
+	},
+	allbanana: {
+		begin: 32.000,
+		onset: 36.001,
+		length: 5.500
+	}, 
+	somecake: {
+		begin: 38.000,
+		onset: 42.376,
+		length: 5.500
+	},
+	nonecake: {
+		begin: 45.000,
+		onset: 49.736,
+		length: 5.500
+	},
+	allcake: {
+		begin: 51.000.
+		onset: 55.695,
+		length: 5.500
+	},
+	somecarrot: {
+		begin: 57.000,
+		onset: 61.686,
+		length: 5.500
+	},
+	nonecarrot: {
+		begin: 63.000,
+		onset: 7.734,
+		length: 6.000
+	},
+	allcarrot: {
+		begin: 70.000,
+		onset: 74.727,
+		length: 5.500
+	},
+	somecookie: {
+		begin: 76.000,
+		onset: 80.222,
+		length: 5.000
+	},
+	nonecookie: {
+		begin: 82.000,
+		onset: 86.693,
+		length: 5.500
+	},
+	allcookie: {
+		begin: 88.000,
+		onset: 92.744,
+		length: 5.500
+	},
+	somedonut: {
+		begin: 94.000,
+		onset: 98.189,
+		length: 5.000
+	},
+	nonedonut: {
+		begin: 100.000,
+		onset: 104.453,
+		length: 5.500
+	},
+	alldonut: {
+		begin: 106.000,
+		onset: 110.346,
+		length: 5.000
+	},
+	someorange: {
+		begin: 112.000,
+		onset: 116.152,
+		length: 5.000
+	},
+	noneorange: {
+		begin: 118.000,
+		onset: 122.388,
+		length: 5.000
+	},
+	allorange: {
+		begin: 124.000,
+		onset: 128.527,
+		length: 5.597
+	},
+	somepear: {
+		begin: 130.000,
+		onset: 134.217,
+		length: 5.000
+	},
+	nonepear: {
+		begin: 136.000,
+		onset:140.247,
+		length: 5.000
+	},
+	allpear: {
+		begin: 142.000,
+		onset: 146.525,
+		length: 5.500
+	},
+	somepizza: {
+		begin: 148.000,
+		onset: 152.109,
+		length: 5.000
+	},
+	nonepizza: {
+		begin: 154.000,
+		onset: 158.291,
+		length: 5.000
+	},
+	allpizza: {
+		begin: 160.000,
+		onset: 164.670,
+		length: 5.500
+	},
+	somepopsicle: {
+		begin: 166.000,
+		onset: 170.153,
+		length: 5.000
+	},
+	nonepopsicle: {
+		begin: 172.000,
+		onset: 176.491,
+		length: 5.200
+	},
+	allpopsicle: {
+		begin: 178.000,
+		onset: 182.430,
+		length: 5.500
+	},
+	somepretzel: {
+		begin: 184.000,
+		onset: 188.154,
+		length: 5.000
+	},
+	nonepretzel: {
+		begin: 190.000,
+		onset: 194.402,
+		length: 5.000
+	},
+	allpretzel: {
+		begin: 196.000,
+		onset: 200.387,
+		length: 5.500
+	},
+	somestrawberry: {
+		begin: 202.000,
+		onset: 206.176,
+		length: 5.200
+	},
+	nonestrawberry: {
+		begin: 208.000,
+		onset: 212.227,
+		length: 5.300
+	},
+	allstrawberry: {
+		begin: 214.000,
+		onset: 218.300,
+		length: 5.500,
+	},
+
+
+};
